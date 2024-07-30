61bde66f881e4912b18c0cc9b2842316db9e569a
Andrés Yépez Calderón <andresyepez966@gmail.com>
2024-07-30 22:34:02 +0200

    implement basic polyline

--- src/fem_glue/geometry/polyline.py
+++ src/fem_glue/geometry/polyline.py
@@ -1,26 +1,44 @@
-from typing import override, Self
-from collections.abc import Iterable
+from typing import override, overload, Self
+from collections.abc import Sequence
 
 from fem_glue.geometry.geometry import Geometry
-from fem_glue.geometry import Line
+from fem_glue.geometry import Line, Point
+from fem_glue.geometry.utils import lines_from_points, points_from_lines
 
 
 class Polyline(Geometry[Line]):
     """
-    A closed non-intersecting polyline with n-straight lines.
+    A polyline with n-straight lines.
     """
-    def __init__(self, elements: Iterable[Line], /):
-        super().__init__(elements)
 
-        # Validate that the polyline is closed and non-intersecting.
-        if self[0] != self[-1]:
-            raise ValueError("The polyline is not closed.")
+    @overload
+    def __init__(self, elements: Sequence[Line], /): ...
 
-        for i, ln1 in enumerate(self):
-            for ln2 in self[i + 1:]:
-                if ln1.intersects(ln2):
-                    raise ValueError("The polyline is intersecting.")
+    @overload
+    def __init__(self, elements: Sequence[Point], /): ...
 
+    def __init__(self, elements: Sequence[Line | Point], /):
+        if all(isinstance(e, Point) for e in elements):
+            # Handle Iterable[Point] constructor
+            elements = lines_from_points(elements)  # type: ignore
+            self.points = list(elements)
+        elif all(isinstance(e, Line) for e in elements):
+            # Handle Iterable[Line] constructor
+            self.points = points_from_lines(elements)  # type: ignore
+        else:
+            # Raise error if elements are not Lines
+            raise ValueError("The elements must be either Points or Lines.")
+
+        super().__init__(elements)  # type: ignore
+
+        # # Validate that the polyline is closed and non-intersecting.
+        # if self[0] != self[-1]:
+        #     raise ValueError("The polyline is not closed.")
+        #
+        # for i, ln1 in enumerate(self):
+        #     for ln2 in self[i + 1 :]:
+        #         if ln1.intersects(ln2):
+        #             raise ValueError("The polyline is intersecting.")
 
     @override
     def __len__(self) -> int:
@@ -31,11 +49,3 @@ class Polyline(Geometry[Line]):
         Calculate the perimeter of the polyline.
         """
         return sum(ln.length() for ln in self)
-
-    def area(self) -> float: ...
-
-    def normalize(self) -> Self:
-        """
-        Normalize the polyline such that it has unit area.
-        """
-
