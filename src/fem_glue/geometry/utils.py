c640261283b39380db4783ce1ce5632634f9c1de
Andrés Yépez Calderón <andresyepez966@gmail.com>
2024-07-30 22:47:22 +0200

    add utils, forgot to do it before

---
+++ src/fem_glue/geometry/utils.py
@@ -0,0 +1,10 @@
+import itertools
+from collections.abc import Sequence
+from fem_glue.geometry import Line, Point
+
+
+def lines_from_points(points: Sequence[Point]) -> list[Line]:
+    return [Line([p1, p2]) for p1, p2 in zip(points, points[1:])]
+
+def points_from_lines(lines: Sequence[Line]) -> list[Point]:
+    return list(itertools.chain.from_iterable(lines))
