019c8b8e6cc64ab5712f4a5f6fb80f3fcf0e07ce
Andrés Yépez Calderón <andresyepez966@gmail.com>
2024-09-15 20:25:49 +0200

    better define operations between geometries and test these operations for Line

--- tests/geometry/test_line.py
+++ tests/geometry/test_line.py
@@ -40,3 +40,66 @@ class TestLine(unittest.TestCase):
     def test_normalize(self):
         normalized_line = self.line1.normalize()
         self.assertEqual(normalized_line.length(), 1)
+
+
+class TestLineOperations(unittest.TestCase):
+    def setUp(self):
+        self.line = Line([Point([3, 4, 5]), Point([6, 8, 10])])
+
+    def test_add(self):
+        # With scalar
+        scalar = 2.5
+        self.assertEqual(
+            self.line + scalar, Line([Point([5.5, 6.5, 7.5]), Point([8.5, 10.5, 12.5])])
+        )
+
+        # With sequence
+        seq = [1, 2.5, 3]
+        self.assertEqual(
+            self.line + seq, Line([Point([4, 6.5, 8]), Point([7, 10.5, 13])])
+        )
+
+    def test_subtract(self):
+        # With scalar
+        scalar = 2.5
+        self.assertEqual(
+            self.line - scalar, Line([Point([0.5, 1.5, 2.5]), Point([3.5, 5.5, 7.5])])
+        )
+
+        # With sequence
+        seq = [1, 2.5, 3]
+        self.assertEqual(
+            self.line - seq, Line([Point([2, 1.5, 2]), Point([5, 5.5, 7])])
+        )
+
+    def test_multiply(self):
+        # With scalar
+        scalar = 2.5
+        self.assertEqual(
+            self.line * scalar, Line([Point([7.5, 10, 12.5]), Point([15, 20, 25])])
+        )
+
+        # With sequence
+        seq = [1, 2.5, 3]
+        self.assertEqual(
+            self.line * seq, Line([Point([3, 10, 15]), Point([6, 20, 30])])
+        )
+
+    def test_divide(self):
+        # With scalar
+        scalar = 2.5
+        self.assertEqual(
+            self.line / scalar, Line([Point([1.2, 1.6, 2]), Point([2.4, 3.2, 4])])
+        )
+
+        # With sequence
+        seq = [1, 2, 4]
+        self.assertEqual(
+            self.line / seq, Line([Point([3, 2, 1.25]), Point([6, 4, 2.5])])
+        )
+
+    def test_pow(self):
+        exp = 2
+        self.assertEqual(
+            self.line**exp, Line([Point([9, 16, 25]), Point([36, 64, 100])])
+        )
