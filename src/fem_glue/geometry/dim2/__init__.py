0d9eb3aa28155ec4c524939dce831632cf985d88
Andrés Yépez Calderón <andresyepez966@gmail.com>
2024-10-06 02:30:27 +0200

    Start work for finding polygon intersections. WORK IN PROGRESS

--- src/fem_glue/geometry/dim2/__init__.py
+++ src/fem_glue/geometry/dim2/__init__.py
@@ -1,3 +1,3 @@
-from fem_glue.geometry.dim2.surface import Surface
+from fem_glue.geometry.dim2.surface import Polygon
 
-__all__ = ["Surface"]
+__all__ = ["Polygon"]
--- src/fem_glue/geometry/dim2/surface.py
+++ src/fem_glue/geometry/dim2/surface.py
@@ -1,3 +1,5 @@
+import math
+import functools
 import numpy as np
 
 from typing import overload
@@ -8,9 +10,9 @@ from fem_glue.geometry._bases import SequentialGeometry
 from fem_glue._config import CONFIG
 
 
-class Surface(SequentialGeometry[Line]):
+class Polygon(SequentialGeometry[Line]):
     """
-    A planar surface in 3D space.
+    A planar polygon in 3D space.
     """
 
     @overload
@@ -32,12 +34,12 @@ class Surface(SequentialGeometry[Line]):
         boundary_elements: Sequence[Line] | Sequence[Point],
         /,
     ):
-        # Surface has closed and non-self-intersecting polyline as boundary
+        # Polygon has closed and non-self-intersecting polyline as boundary
         self.boundary = Polyline(
             boundary_elements, close=True, strict_non_intersecting=True
         )
 
-        # Surface has same sequential geometry behavior as its boundary
+        # Polygon has same sequential geometry behavior as its boundary
         super().__init__(self.boundary)
 
         # Set orthogonal basis of the plane
@@ -50,14 +52,106 @@ class Surface(SequentialGeometry[Line]):
             if line in _basis_computation_lines:
                 continue
 
-            if not self.is_tangent_to_line(line):
+            if not self.line_is_tangent(line):
                 raise ValueError(
                     f"{line} is not tangent to {_basis_computation_lines}."
                 )
 
-    def is_tangent_to_line(self, line: Line) -> bool:
+    @functools.cache
+    def get_plane_coefficients(self) -> tuple[float, float, float, float]:
         """
-        Check if the surface is tangent to the given line.
+        Get the coefficients of the plane equation a, b, c, d.
+        The equation is defined as a*x + b*y + c*z + d = 0.
+
+        Returns
+        -------
+        tuple[float, float, float, float]
+            The coefficients of the plane equation.
+        """
+        a, b, c = self.normal
+        d = -np.dot(self.normal, np.array(Point(self.boundary.points[0])))
+        return a, b, c, d
+
+    def point_on_polygon_plane(self, point: Point) -> bool:
+        """
+        Check if the point is in the same plane as the polygon.
+
+        Parameters
+        ----------
+        point : Point
+            The point to check.
+
+        Returns
+        -------
+        bool
+            True if the point is in the same plane as the polygon, False otherwise.
+        """
+        # If the line formed by the given point and a vertex of the polygon is parallel
+        # to the polygon, then the given point is on the same plane as the polygon
+        # Edge case: if the point is equal to the sampled vertex, point is on plane
+        vertex = self.boundary.points[0]
+        return point == vertex or self.line_is_tangent(Line([point, vertex]))
+
+    def point_on_polygon_boundary(self, point: Point) -> bool:
+        """
+        Check if the point is on the boundary of the polygon.
+
+        Parameters
+        ----------
+        point : Point
+            The point to check.
+
+        Returns
+        -------
+        bool
+            True if the point is on the boundary of the polygon, False otherwise.
+        """
+        # If the point is not on the plane of the polygon, it is not on the boundary
+        if not self.point_on_polygon_plane(point):
+            return False
+
+        # Check if the point is on the boundary of the polygon
+        for edge in self.boundary:
+            if point in edge:
+                return True
+
+    def point_inside_polygon(self, point: Point) -> bool:
+        """
+        Check if the point is inside the polygon (ie. inside its boundary).
+        NOTE: If the point is on the boundary of the polygon, it is not inside of it.
+
+        Parameters
+        ----------
+        point : Point
+            The point to check.
+
+        Returns
+        -------
+        bool
+            True if the point is inside the polygon, False otherwise.
+        """
+        # If the point is not on the plane of the polygon, it is not inside the polygon
+        if not self.point_on_polygon_plane(point):
+            return False
+
+        # Check if the point is inside the polygon
+        # Ray casting algorithm
+        # https://en.wikipedia.org/wiki/Point_in_polygon#Ray_casting_algorithm
+        # Cast a ray from the point in the positive x direction
+        # Count the number of intersections with the polygon boundary
+        # If the number of intersections is odd, the point is inside the polygon
+        # If the number of intersections is even, the point is outside the polygon
+        ray = Line([point, Point([point[0] + 1, point[1], point[2]])])
+        intersections = 0
+        for edge in self.boundary:
+            if ray.intersects(edge):
+                intersections += 1
+
+        return intersections % 2 == 1
+
+    def line_is_tangent(self, line: Line) -> bool:
+        """
+        Check if the polygon is tangent to the given line.
 
         Parameters
         ----------
@@ -67,13 +161,13 @@ class Surface(SequentialGeometry[Line]):
         Returns
         -------
         bool
-            True if the surface is tangent to the line, False otherwise.
+            True if the polygon is tangent to the line, False otherwise.
         """
-        return bool(np.dot(self.normal, line.dir_vector() < CONFIG.tol))
+        return math.isclose(np.dot(self.normal, line.as_vector()), CONFIG.tol)
 
     def _find_orthogonal_basis(self) -> tuple[np.ndarray, tuple[Line, Line]]:
         """
-        Find an orthogonal unit basis for the plane of the surface and the Lines
+        Find an orthogonal unit basis for the plane of the polygon and the Lines
         used to compute this basis.
 
         The first return value is the basis matrix.
@@ -87,7 +181,7 @@ class Surface(SequentialGeometry[Line]):
         tuple[np.ndarray, tuple[Line, Line]]
             The orthogonal basis and the lines used to compute it.
         """
-        # Find the first two non-parallel lines in the surface's boundary
+        # Find the first two non-parallel lines in the polygon's boundary
         first_computation_line = self[0]
         try:
             second_computation_line = next(
@@ -96,7 +190,7 @@ class Surface(SequentialGeometry[Line]):
                 if not line.is_parallel(first_computation_line)
             )
         except StopIteration:
-            raise ValueError("All lines in the surface's boundary are parallel.")
+            raise ValueError("All lines in the polygon's boundary are parallel.")
 
         # Find unit vector tangent to plane and parallel to first computation line
         unit_tangent_1 = first_computation_line.dir_unit_vector()
@@ -104,12 +198,12 @@ class Surface(SequentialGeometry[Line]):
         # Find normal unit vector to plane
         normal = np.cross(
             unit_tangent_1,
-            second_computation_line.dir_vector(),
+            second_computation_line.as_vector(),
         )
         unit_normal = normal / np.linalg.norm(normal)
 
         # Find unit vector tangent to plane and orthogonal to first computation line
-        tangent_2 = np.cross(unit_normal, first_computation_line.dir_vector())
+        tangent_2 = np.cross(unit_normal, first_computation_line.as_vector())
         unit_tangent_2 = tangent_2 / np.linalg.norm(tangent_2)
 
         # Return orthogonal basis
